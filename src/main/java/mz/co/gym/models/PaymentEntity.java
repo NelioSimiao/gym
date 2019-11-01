2c3fceffaf272df3dd976ccc13ba8dd9788b2347
Nelio Simiao MUchisse <neliosimiao@hotmail.com>
2019-11-01 17:27:14 +0200

    dddd

--- src/main/java/mz/co/gym/models/PaymentEntity.java
+++ src/main/java/mz/co/gym/models/PaymentEntity.java
@@ -17,7 +17,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
-
+///
 @Entity
 @Table(name = "payment")
 public class PaymentEntity implements Serializable {
